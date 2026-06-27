2c1fc432c479219450ac7a007e0337eb45cb167f
AfterTime <ywhangus@gmail.com>
2026-06-27 19:31:40 +0800

    fixes

--- vite.config.web.ts
+++ vite.config.web.ts
@@ -5,6 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   root: path.resolve(__dirname, "web"),
+  base: "/NeedMusic/",
   resolve: {
     alias: {
       "@core": path.resolve(__dirname, "src/core"),
